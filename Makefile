999603b70e467ab4e739c84c1c4d401e87e4c930
César Liesens <cliesens@gmail.com>
2021-12-06 01:00:34 +0100

    Rewrite timer scripts & edit other files accordingly

--- Makefile
+++ Makefile
@@ -1,16 +1,17 @@
 all: clean task1 task2
 
+clean: clean1 clean2
+
 task1: compile1 perf1 graphs1
 
 task2: compile2 perf2 graphs2
 
 
+compile: compile1 compile2
+
 compile1: philosophers1 producerConsumer1 readerWriter1
 
 compile2: philosophers2 producerConsumer2 readerWriter2 test_and_set test_test_and_set
-
-
-clean: clean1 clean2
 
 
 philosophers1:
@@ -20,7 +21,7 @@ producerConsumer1:
 readerWriter1:
 		gcc -I -Wall -Werror -std=c99 task_1/readerWriter.c -o out/task_1/readerWriter.o -pthread
 perf1:
-		bash task_1/time_perf.sh
+		./task_1/time_perf.sh out/task_1/philosophers.o && ./task_1/time_perf.sh out/task_1/readerWriter.o && ./task_1/time_perf.sh out/task_1/producerConsumer.o &&
 graphs1:
 		python3 task_1/plot.py
 clean1:
@@ -38,7 +39,7 @@ test_and_set:
 test_test_and_set:
 		gcc -I -Wall -Werror task_2/test_and_test_and_set.c -o out/task_2/test_and_test_and_set.o -pthread
 perf2:
-		bash task_2/time_perf.sh
+		./task_2/time_perf.sh out/task_2/philosophers.o && ./task_2/time_perf.sh out/task_2/readerWriter.o && ./task_2/time_perf.sh out/task_2/producerConsumer.o && ./task_2/time_perf.sh out/task_2/test_and_set.o && ./task_2/time_perf.sh out/task_2/test_and_test_and_set.o
 graphs2:
 		python3 task_2/plot.py && python3 task_2/plot_locks.py 
 clean2:
--- task_2/time_perf.sh
+++ task_2/time_perf.sh
@@ -1,51 +1,45 @@
 #!/bin/bash
 
 # Appel systeme pour le nombre de coeurs
-thread=$(nproc)
+file=$1
+max_threads=$((2 * $(nproc)))
 
-# Effectuer la tache pour chaque fichier ".o"
-for file in out/task_2/*.o;
-do
-	if [ $file != "*.o" ]
+# Initialiser un nouveau fichier csv
+output="${file}.csv"
+echo -n "" > $output
+echo "writing time result in $file.csv"
+
+# Boucle pour les headers
+for ((nb_thread=1;nb_thread<=max_threads;++nb_thread))
+do			
+	if [ $nb_thread == $max_threads ]
 	then
-		# Initialiser un nouveau fichier csv
-		output="${file}.csv"
-		echo -n "" > $output
-		echo "write time result in $file.csv"
-
-		# Boucle pour les headers
-		for ((nb_thread=1;nb_thread<=thread;++nb_thread))
-		do			
-			if [ $nb_thread == $thread ]
-			then
-				echo "$nb_thread," >> $output
-			else
-				echo -n "$nb_thread," >> $output
-			fi
-		done
-
-		# Boucle pour les cinq mesures
-		for i in {1..5}
-		do
-			# Boucle pour chaque thread
-			for ((nb_thread=1;nb_thread<=thread;++nb_thread))
-			do
-				# Mesure du temps d'execution pour les differents fichiers
-				if [ $file == "out/task_2/philosophers.o" ] || [ $file == "out/task_2/test_and_set.o" ]  || [ $file == "out/task_2/test_and_test_and_set.o" ]
-				then
-					time=$(/usr/bin/time -f %e ./$file $nb_thread*2 2>&1|tail -n 1)
-				else
-					time=$(/usr/bin/time -f %e ./$file $nb_thread*2 $nb_thread*2 2>&1|tail -n 1)
-				fi
-				
-				# Ecrire le resultat dans le fichier
-				if [ $nb_thread == $thread ]
-				then
-					echo "$time," >> $output
-				else
-					echo -n "$time," >> $output
-				fi
-			done
-		done
+		echo "$nb_thread," >> $output
+	else
+		echo -n "$nb_thread," >> $output
 	fi
+done
+
+# Boucle pour les cinq mesures
+for i in {1..5}
+do
+	# Boucle pour chaque thread
+	for ((nb_thread=1;nb_thread<=max_threads;++nb_thread))
+	do
+		# Mesure du temps d'execution pour les differents fichiers
+		if [ $file == "out/task_2/philosophers.o" ] || [ $file == "out/task_2/test_and_set.o" ]  || [ $file == "out/task_test_and_test_and_set.o" ]
+		then
+			time=$(/usr/bin/time -f %e ./$file $nb_thread 2>&1|tail -n 1)
+		else
+			time=$(/usr/bin/time -f %e ./$file $nb_thread $nb_thread 2>&1|tail -n 1)
+		fi
+				
+		# Ecrire le resultat dans le fichier
+		if [ $nb_thread == $max_threads ]
+		then
+			echo "$time," >> $output
+		else
+			echo -n "$time," >> $output
+		fi
+	done
 done
